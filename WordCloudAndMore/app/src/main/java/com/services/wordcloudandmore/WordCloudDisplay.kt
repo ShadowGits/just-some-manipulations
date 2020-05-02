13c4d4960142dbd9729e48972de71fed7c69f9e4
ShadowGits <37514710+ShadowGits@users.noreply.github.com>
2020-05-02 16:15:12 +0530

    Python backend maintained .
    Shift to native android
    Initial layout with navigations and listeners achieved

---
+++ WordCloudAndMore/app/src/main/java/com/services/wordcloudandmore/WordCloudDisplay.kt
@@ -0,0 +1,12 @@
+package com.services.wordcloudandmore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordCloudDisplay : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_cloud_display)
+    }
+}
--- word_cloud/lib/constants.dart
+++
@@ -1,1 +0,0 @@
-const String url="http://127.0.0.1:5000/api/Image";
